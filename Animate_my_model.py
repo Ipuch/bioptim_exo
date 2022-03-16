c1458e899865c323bb85455a4beb068dc3654fb4
Aurelien Renou <aurelien.renou@ens2m.org>
2022-03-16 11:34:22 +0100

    Geom and Geometry fusion
    file path changed in kinova_arm_reverse left and right

--- Animate_my_model.py
+++ Animate_my_model.py
@@ -2,7 +2,7 @@ import bioviz
 import numpy as np
 
 # model_name = "KINOVA_arm_deprecated.bioMod"
-model_name = "models/KINOVA_arm_reverse_right.bioMod"
+model_name = "models/KINOVA_arm_reverse_left.bioMod"
 # Load the model - for bioviz
 biorbd_viz = bioviz.Viz(model_name, show_floor=False, show_gravity_vector=False)
 
