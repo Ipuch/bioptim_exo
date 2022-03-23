f008237964e1b38baae4d6c55757cf8d36e831f7
Aurelien Renou <aurelien.renou@ens2m.org>
2022-03-23 16:06:15 +0100

    Correction
    Modification done thanks to commentary

--- set_kinova_in_ulna.py
+++ set_kinova_in_ulna.py
@@ -5,7 +5,6 @@ import biorbd
 # decimal option
 np.set_printoptions(precision=10)
 
-# model_name = "KINOVA_arm_deprecated.bioMod"
 model_name = "models/KINOVA_merge_6dof.bioMod"
 
 biorbd_model = biorbd.Model(model_name)
@@ -45,8 +44,9 @@ Q = np.array(
 print(biorbd_model.nbQ())
 
 # In our configuration, ulna is segment 30 and support is segment 40 ( it can change if KINOVA_merge.bioMod is
-# modify) So, we want to find the rototrans ( aka homogeneous transform) of ulna and the support form local to global
+# modified) So, we want to find the rototrans (aka homogeneous transform) of ulna and the support from local to global
 # coordinates.
+# As a reminder, here is the formula to transform a point from one frame (segment) to another (world).
 # Position_in_world = Rototrans_matrix_world_segment * Position_in_segment
 
 Rototrans_matrix_world_ulna = biorbd_model.globalJCS(Q, 30).to_array()
@@ -56,12 +56,15 @@ Rototrans_matrix_world_support = biorbd_model.globalJCS(Q, 40).to_array()
 # Rototrans_matrix_ulna_support = Rototrans_matrix_ulna_world * Rototrans_matrix_world_support
 
 # Rototrans_matrix_ulna_world is the inverse matrix of Rototrans_matrix_world_ulna, we use .transpose()
+# .transpose() on the Rototrans object which actually inverses the rototranslation matrix.
 Rototrans_matrix_ulna_world = biorbd_model.globalJCS(Q, 30).transpose().to_array()
-# Be aware taht Rototrans_matrix_world_ulna.transpose() would give the wrong matrix
+# Be aware that Rototrans_matrix_world_ulna.transpose() would give the wrong matrix
 
 # Finally
 Rototrans_matrix_ulna_support = np.matmul(Rototrans_matrix_ulna_world, Rototrans_matrix_world_support)
 
 print(Rototrans_matrix_ulna_support)
 
-# we have Rototrans_matrix_ulna_support, so Position_in_ulna = Rototrans_matrix_ulna_support * Position_in_support
+# We have Rototrans_matrix_ulna_support, so, we are now able to transform the coordinates of any point in the support
+# frame to the ulna frame with the following formula:
+# Position_in_ulna = Rototrans_matrix_ulna_support * Position_in_support
