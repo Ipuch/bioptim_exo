ab19a1318c5694ab5b1ec03268b53763ceecdddb
AurelienRenou <aurelien.renou2@ens2m.org>
2022-05-02 11:30:47 -0400

    print information from tracking batch and save it in a csv file

--- tracking/recap.py
+++ tracking/recap.py
@@ -4,11 +4,14 @@ import glob
 import os
 from bioptim import OptimalControlProgram, CostType
 from viz_tracking import add_custom_plots
+import pandas as pd
 
 # data_path = Path("../tracking/")
 os.chdir("../tracking/")
 file_list = []
-for file in glob.glob("save/2022_04_29/*.bo"):  # We get the files names with a .bo extension
+df = pd.DataFrame(columns=["c3d", "time_to_optimize", "iteration", "cost", "statues"])
+file_directory = "2022_04_30"
+for file in glob.glob(f"save/{file_directory}/*.bo"):  # We get the files names with a .bo extension
     file_list.append(file)
 
 for file in file_list:
@@ -17,10 +20,29 @@ for file in file_list:
     ocp, sol = OptimalControlProgram.load(file)
     ocp.add_plot_penalty(CostType.ALL)
     list_markers = ["MET5", "CLAV_SC"]
-    ocp = add_custom_plots(ocp, list_markers)
-    sol.graphs()
     print("time to optimize:", sol.real_time_to_optimize / 60)
     print("iteration:", sol.iterations)
     print("cost:", sol.cost)
     print("status:", sol.status)
+    if sol.status == 1:
+        print("let's see the graphs")
+        # ocp = add_custom_plots(ocp, list_markers)
+        # sol.graphs()
     print("--------------")
+    cur_dict = dict(c3d=file,
+                    time_to_optimize=sol.real_time_to_optimize / 60,
+                    iteration=sol.iterations,
+                    cost=float(sol.cost),
+                    status=sol.status)
+    row_df = pd.DataFrame([cur_dict])
+    df = pd.concat([df, row_df], ignore_index=True)
+
+file_names = []
+i = 0
+# if another file has the same name, it indents the name with 0, 1, ...
+for file in glob.glob(f"save/csv_save/*{file_directory}*"):
+    file_names.append(file)
+
+for file in file_names:
+    i += 1
+df.to_csv(f"save/csv_save/recap_{file_directory}_{i}")
