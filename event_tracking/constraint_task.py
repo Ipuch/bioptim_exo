b288a4a898484b639a1815ef5717d9346259c87a
thasaarah <thasaarah@gmail.com>
2022-05-30 09:31:18 -0400

    checkpoint

--- event_tracking/constraint_task.py
+++ event_tracking/constraint_task.py
@@ -10,7 +10,6 @@ from bioptim import (
     ObjectiveFcn,
     QAndQDotBounds,
     OdeSolver,
-    Node,
     Dynamics,
     InitialGuess,
     Bounds,
@@ -33,54 +32,45 @@ import load_experimental_data
 
 
 def prepare_ocp(
-    biorbd_model_path: str,
-    c3d_file_path: str,
-    n_shooting: int,
-    x_init_ref: np.array,
-    u_init_ref: np.array,
-    target_start: any,
-    target_end: any,
-    # LoadEvent: object = LoadEvent(c3d_path="F0_tete_05.c3d"),
-    ode_solver: OdeSolver = OdeSolver.RK4(),
-    use_sx: bool = False,
-    n_threads: int = 4,
+        biorbd_model_path: str,
+        n_shooting: int,
+        x_init_ref: np.array,
+        u_init_ref: np.array,
+        ode_solver: OdeSolver = OdeSolver.RK4(),
+        use_sx: bool = False,
+        n_threads: int = 4,
+        phase_time: float = 1,
 ) -> object:
     biorbd_model = biorbd.Model(biorbd_model_path)
 
     # Add objective functions
     objective_functions = ObjectiveList()
-    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", weight=1000)  # 100
-    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_STATE, weight=1, key="qdot")
-    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_STATE, key="q", index=[0, 1], weight=10)
+    # objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", index=[5, 6, 7, 8, 9, 10], weight=1000)
+    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", index=range(5, 9), weight=1000)
+    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_STATE, key="qdot", weight=1)
+    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_STATE, key="q", index=[0, 1, 2, 3, 4], weight=1000)
 
     # Dynamics
     dynamics = Dynamics(DynamicsFcn.TORQUE_DRIVEN)
 
-    # Path constraint
-    x_bounds = QAndQDotBounds(biorbd_model)
-
+    # initial guesses
     x_init = InitialGuess(x_init_ref, interpolation=InterpolationType.EACH_FRAME)
     u_init = InitialGuess(u_init_ref, interpolation=InterpolationType.EACH_FRAME)
 
-    Q = biorbd.GeneralizedCoordinates(biorbd_model)
-    Qdot = biorbd.GeneralizedVelocity(biorbd_model)
-    Qddot = biorbd.GeneralizedAcceleration(biorbd_model)
-
-
-
     # Define control path constraint
+    x_bounds = QAndQDotBounds(biorbd_model)
+    x_bounds[:10, 0] = x_init_ref[:10, 0]
+    x_bounds[:10, -1] = x_init_ref[:10, -1]
+
     n_tau = biorbd_model.nbGeneralizedTorque()
     tau_min, tau_max = -100, 100
     u_bounds = Bounds([tau_min] * n_tau, [tau_max] * n_tau)
-    # u_bounds[1, :] = 0
-
-    u_init = InitialGuess([0] * n_tau)
 
     return OptimalControlProgram(
         biorbd_model,
         dynamics,
         n_shooting,
-        phase_time=1,
+        phase_time=phase_time,
         x_init=x_init,
         u_init=u_init,
         x_bounds=x_bounds,
@@ -98,16 +88,8 @@ def main():
     """
 
     # Define the problem
-    # with_floating_base = False
-
     c3d_path = "F0_dents_05.c3d"
-    # todo: manger, aisselle, dessiner
-    c3d = ezc3d.c3d(c3d_path)
-    freq = c3d["parameters"]["POINT"]["RATE"]["value"][0]
-    nb_frames = c3d["parameters"]["POINT"]["FRAMES"]["value"][0]
-    duration = nb_frames / freq
-    # n_shooting_points = int(duration * 100)
-    # n_shooting_points = 100
+    n_shooting_points = 100
     nb_iteration = 10000
 
     q_file_path = c3d_path.removesuffix(".c3d") + "_q.txt"
@@ -121,46 +103,45 @@ def main():
     biorbd_model = biorbd.Model(new_biomod_file)
     marker_ref = [m.to_string() for m in biorbd_model.markerNames()]
 
+    # get key events
     event = load_events.LoadEvent(c3d_path=c3d_path, marker_list=marker_ref)
-
-    target_start = event.get_markers(0)[:, :, np.newaxis]
-    target_end = event.get_markers(1)[:, :, np.newaxis]
-
     data = load_experimental_data.LoadData(biorbd_model, c3d_path, q_file_path, qdot_file_path)
     start_frame = event.get_frame(0)
     end_frame = event.get_frame(1)
+    phase_time = event.get_time(1)-event.get_time(0)
 
-    n_shooting_points = 100
-    phase_time = 1
-    tau = data.tau
-    q_ref, qdot_ref, tau_ref = data.get_states_ref(
+    # load initial guesses
+    q_ref, qdot_ref, tau_ref = data.get_variables_ref(
         number_shooting_points=[n_shooting_points],
         phase_time=[phase_time],
         start=start_frame,
         end=end_frame,
     )
-    x_init_ref = np.concatenate([q_ref[0][6:], qdot_ref[0][6:]]) # without floating base
-    u_init_ref = tau_ref[0][6:]
+    x_init_ref = np.concatenate([q_ref[0][6:, :], qdot_ref[0][6:, :]])  # without floating base
+    u_init_ref = tau_ref[0][6:, :]
+
+    # optimal control program
     my_ocp = prepare_ocp(
         biorbd_model_path=new_biomod_file,
-        c3d_file_path=c3d_path,
         x_init_ref=x_init_ref,
         u_init_ref=u_init_ref,
-        target_start=target_start,
-        target_end=target_end,
         n_shooting=n_shooting_points,
-        ode_solver=OdeSolver.RK4(),
         use_sx=False,
         n_threads=4,
+        phase_time=phase_time,
     )
+
+    # my_ocp.print()
+
+    # add figures of constraints and objectives
     my_ocp.add_plot_penalty(CostType.ALL)
+
     # --- Solve the program --- #
     solver = Solver.IPOPT(show_online_optim=True, show_options=dict(show_bounds=True))
     solver.set_linear_solver("ma57")
     solver.set_maximum_iterations(nb_iteration)
     sol = my_ocp.solve(solver)
     sol.print_cost()
-    # sol.print_cost(
 
     # --- Save --- #
     c3d_str = c3d_path.split("/")
