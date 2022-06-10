759d6a13436995907b81537e8fece0a4ece7a2d2
AurelienRenou <aurelien.renou@ens2m.org>
2022-06-10 15:01:09 -0400

    inverse kinematics of wu model

--- kinova_tracking/inverse_kinematics_run.py
+++ kinova_tracking/inverse_kinematics_run.py
@@ -5,8 +5,8 @@ from ezc3d import c3d
 import biorbd
 import os
 
-model_path = "../models/KINOVA_merge_without_floating_base.bioMod"
-model_path = "../models/wu_converted_definitif_without_floating_base.bioMod"
+# model_path = "../models/KINOVA_merge_without_floating_base.bioMod"
+model_path = "../models/wu_converted_definitif_inverse_kinematics.bioMod"
 biorbd_model = biorbd.Model(model_path)
 
 c3d_path = "../data/kinova_arm/F3_aisselle_01.c3d"
@@ -33,7 +33,7 @@ for i, name in enumerate(marker_names):
 # markers = markers[:, :, 0:10]
 my_ik = ik.InverseKinematics(model_path, markers)
 start_c3d = time.time()
-my_ik.solve("trf")
+my_ik.solve()
 end_c3d = time.time()
 print("The time used to execute with least square and with a c3d file instead of a c3d path of markers is given below")
 print(end_c3d - start_c3d)
