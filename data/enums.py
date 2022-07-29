3c7829a7aa9028bcbabd28b41cbc2cbbb828501e
AurelienRenou <aurelien.renou@ens2m.org>
2022-07-29 14:56:04 -0400

    fix enums

--- data/enums.py
+++ data/enums.py
@@ -7,12 +7,12 @@ class Tasks(Enum):
     Selection of tasks
     """
 
-    ARMPIT = str(Path("F0_aisselle_05.c3d").absolute())
-    DRINK = str(Path("F0_boire_05.c3d").absolute())
-    TEETH = str(Path("F0_dents_05.c3d").absolute())
-    DRAW = str(Path("F0_dessiner_05.c3d").absolute())
-    EAT = str(Path("F0_manger_05.c3d").absolute())
-    HEAD = str(Path("F0_tete_05.c3d").absolute())
+    ARMPIT = Path(__file__).parent.__str__() + "/F0_aisselle_05.c3d"
+    DRINK = Path(__file__).parent.__str__() + "/F0_boire_05.c3d"
+    TEETH = Path(__file__).parent.__str__() + "/F0_dents_05.c3d"
+    DRAW = Path(__file__).parent.__str__() + "/F0_dessiner_05.c3d"
+    EAT = Path(__file__).parent.__str__() + "/F0_manger_05.c3d"
+    HEAD = Path(__file__).parent.__str__() + "/F0_tete_05.c3d"
 
 
 class TasksKinova(Enum):
@@ -20,11 +20,11 @@ class TasksKinova(Enum):
     Selection of tasks
     """
 
-    ARMPIT = str(Path("F3_aisselle_01.c3d").absolute())
-    DRINK = str(Path("F3_boire_01.c3d").absolute())
-    TEETH = str(Path("F3_dents_01.c3d").absolute())
-    DRAW = str(Path("F3_dessiner_02.c3d").absolute())
-    HEAD = str(Path("F3_tete_01.c3d").absolute())
+    ARMPIT = Path(__file__).parent.__str__() + "/F3_aisselle_01.c3d"
+    DRINK = Path(__file__).parent.__str__() + "/F3_boire_01.c3d"
+    TEETH = Path(__file__).parent.__str__() + "/F3_dents_01.c3d"
+    DRAW = Path(__file__).parent.__str__() + "/F3_dessiner_02.c3d"
+    HEAD = Path(__file__).parent.__str__() + "/F3_tete_01.c3d"
 
 
 
--- models/enums.py
+++ models/enums.py
@@ -7,35 +7,29 @@ class Models(Enum):
     Selection of Model
     """
 
-    KINOVA_DEPRECATED = str(Path("KINOVA_arm_deprecated.bioMod").absolute())
-    KINOVA_LEFT = str(Path("KINOVA_arm_reverse_left.bioMod").absolute())
-    KINOVA_RIGHT = str(Path("KINOVA_arm_reverse_right.bioMod").absolute())
+    KINOVA_DEPRECATED = Path(__file__).parent.__str__() + "/KINOVA_arm_deprecated.bioMod"
+    KINOVA_LEFT = Path(__file__).parent.__str__() + "/KINOVA_arm_reverse_left.bioMod"
+    KINOVA_RIGHT = Path(__file__).parent.__str__() + "/KINOVA_arm_reverse_right.bioMod"
 
-    WU = str(Path("wu_converted_definitif.bioMod").absolute())
-    WU_INVERSE_KINEMATICS = str(Path("wu_converted_definitif_inverse_kinematics.bioMod").absolute())
-    WU_INVERSE_KINEMATICS_XYZ = str(Path("wu_converted_definitif_inverse_kinematics_XYZ.bioMod").absolute())
-    WU_WITHOUT_FLOATING_BASE = str(Path("wu_converted_definitif_without_floating_base.bioMod").absolute())
-    WU_WITHOUT_FLOATING_BASE_TEMPLATE = str(Path("wu_converted_definitif_without_floating_base_template.bioMod").absolute())
-    WU_WITHOUT_FLOATING_BASE_FIXED_TEMPLATE = str(Path("wu_converted_definitif_without_floating_base_template_fixed.bioMod").absolute())
-    WU_WITHOUT_FLOATING_BASE_QUAT_TEMPLATE = str(Path("wu_converted_definitif_without_floating_base_template_quat.bioMod").absolute())
-    WU_WITHOUT_FLOATING_BASE_VARIABLES = str(Path("wu_converted_definitif_without_floating_base_template_with_variables.bioMod").absolute())
-    WU_WITHOUT_MODIF = str(Path("wu_converted_definitif_without_modif.bioMod").absolute())
-    WU_WITHOUT_MODIF_QUAT = str(Path("wu_converted_definitif_without_modif_quat.bioMod").absolute())
+    WU = Path(__file__).parent.__str__() + "/wu_converted_definitif.bioMod"
+    WU_INVERSE_KINEMATICS = Path(__file__).parent.__str__() + "/wu_converted_definitif_inverse_kinematics.bioMod"
+    WU_INVERSE_KINEMATICS_XYZ = Path(__file__).parent.__str__() + "/wu_converted_definitif_inverse_kinematics_XYZ.bioMod"
+    WU_WITHOUT_FLOATING_BASE = Path(__file__).parent.__str__() + "/wu_converted_definitif_without_floating_base.bioMod"
+    WU_WITHOUT_FLOATING_BASE_TEMPLATE = Path(__file__).parent.__str__() + "/wu_converted_definitif_without_floating_base_template.bioMod"
+    WU_WITHOUT_FLOATING_BASE_FIXED_TEMPLATE = Path(__file__).parent.__str__() + "/wu_converted_definitif_without_floating_base_template_fixed.bioMod"
+    WU_WITHOUT_FLOATING_BASE_QUAT_TEMPLATE = Path(__file__).parent.__str__() + "/wu_converted_definitif_without_floating_base_template_quat.bioMod"
+    WU_WITHOUT_FLOATING_BASE_VARIABLES = Path(__file__).parent.__str__() + "/wu_converted_definitif_without_floating_base_template_with_variables.bioMod"
+    WU_WITHOUT_MODIF = Path(__file__).parent.__str__() + "/wu_converted_definitif_without_modif.bioMod"
+    WU_WITHOUT_MODIF_QUAT = Path(__file__).parent.__str__() + "/wu_converted_definitif_without_modif_quat.bioMod"
 
-    WU_AND_KINOVA = str(Path("KINOVA_merge.bioMod").absolute())
-    WU_AND_KINOVA_INVERSE_KINEMATICS = str(Path("KINOVA_merge_inverse_kinematics.bioMod").absolute())
+    WU_AND_KINOVA = Path(__file__).parent.__str__() + "/KINOVA_merge.bioMod"
+    WU_AND_KINOVA_INVERSE_KINEMATICS = Path(__file__).parent.__str__() + "/KINOVA_merge_inverse_kinematics.bioMod"
 
-    WU_AND_KINOVA_WITHOUT_FLOATING_BASE_TEMPLATE = str(
-        Path("KINOVA_merge_without_floating_base_template.bioMod").absolute())
-    WU_AND_KINOVA_WITHOUT_FLOATING_BASE_WITH_VARIABLES = str(
-        Path("KINOVA_merge_without_floating_base_template_with_variables.bioMod").absolute())
+    WU_AND_KINOVA_WITHOUT_FLOATING_BASE_TEMPLATE = Path(__file__).parent.__str__() + "/KINOVA_merge_without_floating_base_template.bioMod"
+    WU_AND_KINOVA_WITHOUT_FLOATING_BASE_WITH_VARIABLES = Path(__file__).parent.__str__() + "/KINOVA_merge_without_floating_base_template_with_variables.bioMod"
 
-    WU_AND_KINOVA_WITHOUT_FLOATING_BASE_WITH_6_DOF_SUPPORT_TEMPLATE = str(
-        Path("KINOVA_merge_without_floating_base_with_6_dof_support_template.bioMod").absolute())
-    WU_AND_KINOVA_WITHOUT_FLOATING_BASE_WITH_6_DOF_SUPPORT_VARIABLES = str(
-        Path("KINOVA_merge_without_floating_base_with_6_dof_support_template_with_variables.bioMod").absolute())
+    WU_AND_KINOVA_WITHOUT_FLOATING_BASE_WITH_6_DOF_SUPPORT_TEMPLATE = Path(__file__).parent.__str__() + "/KINOVA_merge_without_floating_base_with_6_dof_support_template.bioMod"
+    WU_AND_KINOVA_WITHOUT_FLOATING_BASE_WITH_6_DOF_SUPPORT_VARIABLES = Path(__file__).parent.__str__() + "/KINOVA_merge_without_floating_base_with_6_dof_support_template_with_variables.bioMod"
 
-    WU_AND_KINOVA_WITHOUT_FLOATING_BASE_WITH_ROTOTRANS_SUPPORT_TEMPLATE = str(
-        Path("KINOVA_merge_without_floating_base_with_rototrans_template.bioMod").absolute())
-    WU_AND_KINOVA_WITHOUT_FLOATING_BASE_WITH_ROTOTRANS_SUPPORT_VARIABLES = str(
-        Path("KINOVA_merge_without_floating_base_with_rototrans_template_with_variables.bioMod").absolute())
+    WU_AND_KINOVA_WITHOUT_FLOATING_BASE_WITH_ROTOTRANS_SUPPORT_TEMPLATE = Path(__file__).parent.__str__() + "/KINOVA_merge_without_floating_base_with_rototrans_template.bioMod"
+    WU_AND_KINOVA_WITHOUT_FLOATING_BASE_WITH_ROTOTRANS_SUPPORT_VARIABLES = Path(__file__).parent.__str__() + "/KINOVA_merge_without_floating_base_with_rototrans_template_with_variables.bioMod"
